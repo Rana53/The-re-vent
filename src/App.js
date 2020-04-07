42295c99b305f7c74fbf2e4d45bddf6eca154040
Rana53 <ranajahid.cseru@gmail.com>
2020-04-08 01:18:50 +0600

    A Social event web application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import EventDashboard from '../src/features/event/EventDashboard/EventDashboard';
+import NavBar from './features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar/>
+      <Container className="main">
+        <EventDashboard/>
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
