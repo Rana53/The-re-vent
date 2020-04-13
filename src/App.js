5891ac38de37b85dd766fb4bf62d91a3edd18594
Rana53 <ranajahid.cseru@gmail.com>
2020-04-13 21:58:37 +0600

    Home page setup and destructured route

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ import { Route } from 'react-router-dom';
 import  HomePage  from './features/home/HomePage';
 import EventDashboard from './features/events/EventDashboard/EventDashboard';
 import EventDetailedPage from './features/events/EventDetailed/EventDetailedPage';
-import PeopleDashboard from './features/user/PeopleDashboard/PeopleDashboard';
+import PeopleDashboard from './features/user/PeopleDashboard/PeopleDashboard'; 
 import UserDetailedPage from './features/user/UserDetailed/UserDetailedPage';
 import SettingsDashboard from './features/user/Seetings/SettingsDashboard';
 import EventForm from './features/events/EventForm/EventForm';
 
 function App() {
   return (
-    <Fragment> 
-      <NavBar/>
-      <Container className="main">
-        <Route path='/' exact component={HomePage}/>
-        <Route path='/events' component={EventDashboard}/>
-        <Route path='/events/:id' component={EventDetailedPage}/>
-        <Route path='/people' component={PeopleDashboard}/>
-        <Route path='/profile/:id' component={UserDetailedPage}/>
-        <Route path='/seetings' component={SettingsDashboard}/>
-        <Route path='/createEvent' component={EventForm}/>
-      </Container>
+    <Fragment>
+      <Route path='/' exact component={HomePage}/>
+      <Route
+        path='/(.+)'
+        render={() => (
+          <Fragment> 
+            <NavBar/>
+            <Container className="main">
+              <Route path='/events' component={EventDashboard}/>
+              <Route path='/events/:id' component={EventDetailedPage}/>
+              <Route path='/people' component={PeopleDashboard}/>
+              <Route path='/profile/:id' component={UserDetailedPage}/>
+              <Route path='/seetings' component={SettingsDashboard}/>
+              <Route path='/createEvent' component={EventForm}/>
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
+    
   );
 }
 
